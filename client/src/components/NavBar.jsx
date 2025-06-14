1310a09599c3582b99bf303c3b6c6b688d12425b
Timolino28 <kordmann@web.de>
2025-06-14 19:12:04 +0200

    Iterating navbar design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 function NavBar() {
   return (
-    <div className="navbar bg-neutral-800 shadow-sm">
+    <div className="navbar min-h-25 bg-neutral-800 border-b-2 border-solid border-bottom border-green-400">
       <div className="navbar-start">
         <a className="text-xl font-semibold text-green-400 ml-5" href="/">
           Zero Yield Club 💸
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li className="font-semibold text-green-400">
+        <ul className="menu menu-horizontal px-1 gap-10">
+          <li className="text-xl font-semibold text-green-400">
             <a href="/calendar">Dividend Calendar</a>
           </li>
-          <li className="font-semibold text-green-400">
-            <a>Item 3</a>
+          <li className="text-xl font-semibold text-green-400">
+            <a>Valuation Check</a>
           </li>
         </ul>
       </div>
