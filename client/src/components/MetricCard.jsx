ada26d7f9452c4fcfdf6506c9758cbd1b71a20fe
Timolino28 <kordmann@web.de>
2025-06-19 10:30:25 +0200

    Adding tooltip to stock metrics

--- client/src/components/MetricCard.jsx
+++ client/src/components/MetricCard.jsx
@@ -9,6 +9,7 @@ import { getOverallValuationScore } from "../utils/getOverallValuationScore";
 import UndervaluedBadge from "../components/UndervaluedBadge";
 import OvervaluedBadge from "../components/OvervaluedBadge";
 import NeutralBadge from "../components/NeutralBadge";
+import { MdInfoOutline } from "react-icons/md";
 
 function renderBadge(score) {
   if (score === "undervalued") return <UndervaluedBadge />;
@@ -46,7 +47,7 @@ function MetricCard({ data, isLoading, isError }) {
 
   return (
     <>
-      <div class="stats shadow-md shadow-green-400 bg-neutral-800">
+      <div class="stats shadow-md shadow-green-400 bg-neutral-800 overflow-visible">
         <div class="stat border-r border-green-400">
           <div class="stat-title text-neutral-300">Current Stock Price</div>
           <div class="stat-value text-white">
@@ -58,7 +59,16 @@ function MetricCard({ data, isLoading, isError }) {
           <div class="stat-figure">
             {renderBadge(getDCFScore(data?.dcf, data?.stockPrice))}
           </div>
-          <div class="stat-title text-neutral-300">Current DCF-Value</div>
+          <div class="stat-title text-neutral-300">
+            {" "}
+            <div
+              className="tooltip tooltip-success flex"
+              data-tip="The DCF model estimates the present value of a company based on its expected future cash flows, discounted back to today using a required rate of return and therfore determines the intrinsic value of a stock."
+            >
+              Current DCF-Value
+              <MdInfoOutline />
+            </div>
+          </div>
           <div class="stat-value text-white">
             {data.dcf != null ? data.dcf : "N/A"}
           </div>
@@ -69,7 +79,14 @@ function MetricCard({ data, isLoading, isError }) {
             {renderBadge(getPEScore(data?.priceToEarningsRatio))}
           </div>
           <div class="stat-title text-neutral-300">
-            Price-to-Earnings Ratio (P/E)
+            <div
+              className="tooltip tooltip-success flex"
+              data-tip="The P/E ratio compares a company’s current share price to its earnings per share (EPS)
+ and therfore indicates how much investors are willing to pay for each unit of earnings."
+            >
+              Price-to-Earnings Ratio (P/E)
+              <MdInfoOutline />
+            </div>
           </div>
           <div class="stat-value text-white">
             {data.priceToEarningsRatio != null
@@ -83,7 +100,14 @@ function MetricCard({ data, isLoading, isError }) {
             {renderBadge(getPBScore(data?.priceToBookRatio))}
           </div>
           <div class="stat-title text-neutral-300">
-            Price-to-Book Ratio (P/B)
+            <div
+              className="tooltip tooltip-success flex"
+              data-tip="The P/B ratio compares a company’s market value to its book value and therfore reflects how the market values a company's net assets.
+"
+            >
+              Price-to-Book Ratio (P/B)
+              <MdInfoOutline />
+            </div>
           </div>
           <div class="stat-value text-white">
             {data.priceToBookRatio != null ? data.priceToBookRatio : "N/A"}
@@ -95,7 +119,14 @@ function MetricCard({ data, isLoading, isError }) {
             {renderBadge(getDEScore(data?.debtToEquityRatio))}
           </div>
           <div class="stat-title text-neutral-300">
-            Debt-to-Equity Ratio (D/E)
+            <div
+              className="tooltip tooltip-success flex"
+              data-tip="The D/E ratio measures a company’s financial leverage by comparing total debt to shareholder equity and therfore shows how much a company relies on debt to finance its assets.
+"
+            >
+              Debt-to-Equity Ratio (D/E)
+              <MdInfoOutline />
+            </div>
           </div>
           <div class="stat-value text-white">
             {data.debtToEquityRatio != null ? data.debtToEquityRatio : "N/A"}
