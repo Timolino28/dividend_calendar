e9e8c951c6a8ea26ec60aac5e2fe8a5da79e9d7e
Timolino28 <kordmann@web.de>
2025-06-22 10:56:06 +0200

    Further developing landingpage

--- client/src/components/Landingpage/FAQ.jsx
+++ client/src/components/Landingpage/FAQ.jsx
@@ -3,33 +3,33 @@ import React from "react";
 function FAQ() {
   return (
     <div className="bg-neutral-800 pt-10">
-      <div className="join join-vertical bg-base-100">
+      <div className="flex max-w-3xl mx-auto join join-vertical bg-base-800 p-10">
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title font-semibold">
+          <div className="text-white collapse-title font-semibold">
             How do I create an account?
           </div>
-          <div className="collapse-content text-sm">
+          <div className="text-white collapse-content text-sm">
             Click the "Sign Up" button in the top right corner and follow the
             registration process.
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title font-semibold">
+          <div className="text-white collapse-title font-semibold">
             I forgot my password. What should I do?
           </div>
-          <div className="collapse-content text-sm">
+          <div className="text-white collapse-content text-sm">
             Click on "Forgot Password" on the login page and follow the
             instructions sent to your email.
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title font-semibold">
+          <div className="text-white collapse-title font-semibold">
             How do I update my profile information?
           </div>
-          <div className="collapse-content text-sm">
+          <div className="text-white collapse-content text-sm">
             Go to "My Account" settings and select "Edit Profile" to make
             changes.
           </div>
--- client/src/components/Landingpage/Hero.jsx
+++ client/src/components/Landingpage/Hero.jsx
@@ -1,22 +1,19 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 function Hero() {
   return (
-    <div className="hero bg-neutral-800 py-5">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
+    <div className=" bg-neutral-800 ">
+      <div className="hero-content mx-auto py-5 text-center">
+        <div className="max-w-xl mt-10">
           <h1 className="text-4xl text-green-400 font-black font-roboto">
             The Hunting List for future{" "}
-            <span className="uppercase italic tracking-wide leading-[1.5] text-5xl text-neutral-800 relative inline-block fancy-highlight">
+            <span className="mt-3 uppercase italic tracking-wide leading-[1.5] text-5xl text-neutral-800 relative inline-block fancy-highlight">
               Millionaires!
             </span>
           </h1>
-          <p className="text-white text-[18px] py-6">
-            Save prospect stocks, track their performance and build the
-            foundation for one monkey-mad portfolio
-          </p>
           <div
-            class="tenor-gif-embed mx-auto"
+            class="tenor-gif-embed mx-auto my-10"
             data-postid="14116367"
             data-share-method="host"
             data-aspect-ratio="1.36806"
@@ -35,6 +32,14 @@ function Hero() {
             async
             src="https://tenor.com/embed.js"
           ></script>
+          <p className="text-neutral-200 text-xl font-semibold py-4 mb-5">
+            Save prospect stocks, track their performance and build the
+            foundation for one monkey-mad portfolio
+          </p>
+          <button className="btn text-neutral-800 bg-green-400 border border-green-400 text-xl py-6 px-8">
+            Let's Start
+            <FaArrowRight />
+          </button>
         </div>
       </div>
     </div>
