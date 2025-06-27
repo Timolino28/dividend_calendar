ae4b5d72c4ccd8fb297f500c4d13deb5fb32f702
Timolino28 <kordmann@web.de>
2025-06-27 06:20:15 +0200

    Adjusting FAQ section

--- client/src/components/Landingpage/FAQ.jsx
+++ client/src/components/Landingpage/FAQ.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function FAQ() {
   return (
     <div className="bg-neutral-800 py-20">
-      <div className="max-w-5xl flex justify-center mx-auto px-10 py-10">
-        <div className="max-w-3xl">
-          <h1 className="text-3xl font-semibold text-neutral-100">
+      <div className="max-w-6xl gap-10 flex justify-center mx-auto px-10 py-10">
+        <div className="w-full">
+          <h1 className="text-3xl font-semibold text-neutral-100 whitespace-nowrap">
             Frequently asked questions
           </h1>
         </div>
-        <div className="flex max-w-3xl mx-auto join join-vertical bg-base-800 px-10">
+        <div className="flex mx-auto join join-vertical bg-base-800 px-10">
           <div className="collapse collapse-arrow join-item border-base-300 border">
             <input type="radio" name="my-accordion-4" defaultChecked />
             <div className="text-white collapse-title font-semibold">
@@ -17,9 +17,9 @@ function FAQ() {
             </div>
             <div className="text-white collapse-content text-sm">
               The Zero Yield Club was created to help private investors discover
-              new stock opportunities. Since we know how time-consuming and
-              tedious this can be, we built this tool to do the heavy lifting —
-              for you and for us.
+              new stock opportunities and decide whether to invest or keep
+              looking. Since we know how time-consuming and tedious this can be,
+              we built this tool to do the heavy lifting — for you and for us.
             </div>
           </div>
           <div className="collapse collapse-arrow join-item border-base-300 border">
